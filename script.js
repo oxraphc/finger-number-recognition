472008bee6051b14d87b7906f31cfb60c3849a94
Oxraphc <caesarravsanjani170@gmail.com>
2025-11-23 12:55:20 +0700

    Detect hand landmarks

--- script.js
+++ script.js
@@ -1,7 +1,50 @@
-const camera = document.getElementById('camera')
+import {
+  GestureRecognizer,
+  FilesetResolver,
+  DrawingUtils
+} from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3";
+
+
+const camera = document.getElementById('camera');
+const drawOverlay = document.getElementById('draw-overlay');
+const drawOverlayContext = drawOverlay.getContext('2d');
+const leftHandCVResultOutput = document.getElementById('left-hand-result');
+const rightHandCVResultOutput = document.getElementById('right-hand-result');
+
+let cameraStream = null;
+let gestureRecognizer;
+let lastVideoTime = -1;
+let results = undefined;
+
+let left_value = 0;
+let right_value = 0;
+let operation = '+';
+let result = 0;
+
+
+// Load Model
+console.log("Begin loading model.")
+const createGestureRecognizer = async () => {
+  const vision = await FilesetResolver.forVisionTasks(
+    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
+  );
+  gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
+    baseOptions: {
+      modelAssetPath:
+        "model/finger_number_recognizer_v4.task",
+      delegate: "GPU"
+    },
+    runningMode: 'VIDEO',
+    numHands: 2
+  });
+};
+await createGestureRecognizer();
+console.log("Finished loading.")
+
 
 async function openCamera() {
-    try {
+    return new Promise(async (resolve) => {
+        try {
         cameraStream = await navigator.mediaDevices.getUserMedia({
                 video: {
                     aspectRatio: 16 / 9,
@@ -11,10 +54,67 @@ async function openCamera() {
         camera.srcObject = cameraStream;
         await new Promise(resolve => camera.onloadedmetadata = resolve);
         camera.play();
-        console.log('Camera opened.')
-        console.log(`Camera dimensions : ${camera.videoWidth} x ${camera.videoHeight}`)
+        console.log('Camera opened.');
+        console.log(`Camera dimensions : ${camera.videoWidth} x ${camera.videoHeight}`);
+        resolve(true);
     } catch (e) {
         console.error('Error accessing camera: ', e);
+        resolve(false);
     }
+    })
 }
-openCamera();
+if (await openCamera()) {
+    predictWebcam();
+}
+
+
+async function predictWebcam() {
+    // Start detecting the stream.
+    let nowInMs = Date.now();
+    if (camera.currentTime !== lastVideoTime) {
+      lastVideoTime = camera.currentTime;
+      results = gestureRecognizer.recognizeForVideo(camera, nowInMs);
+    }
+
+    drawOverlayContext.save();
+    drawOverlayContext.clearRect(0, 0, drawOverlay.width, drawOverlay.height);
+    const drawingUtils = new DrawingUtils(drawOverlayContext);
+
+    if (results.landmarks) {
+      for (const landmarks of results.landmarks) {
+        drawingUtils.drawConnectors(
+          landmarks,
+          GestureRecognizer.HAND_CONNECTIONS,
+          {
+            color: "#00FF00",
+            lineWidth: 1
+          }
+        );
+        drawingUtils.drawLandmarks(landmarks, {
+          color: "#FF0000",
+          lineWidth: 0,
+          radius: 1
+        });
+      }
+    }
+    drawOverlayContext.restore();
+    // if (results.gestures.length > 0) {
+    //   gestureOutput.style.display = "block";
+    //   gestureOutput.style.width = videoWidth;
+    //   const categoryName = results.gestures[0][0].categoryName;
+    //   const categoryName2 = null;
+    //   const categoryScore = parseFloat(
+    //     results.gestures[0][0].score * 100
+    //   ).toFixed(2);
+    //   const handedness = results.handednesses[0][0].displayName;
+    //   gestureOutput.innerText = `results.gestures[1] = ${results.gestures[0][0]}\n\nGestureRecognizer1: ${categoryName}\n GestureRecognizer2: ${categoryName2}\n  Confidence: ${categoryScore} %\n Handedness: ${handedness}`;
+
+    //   if (results.gestures.length > 1) {
+    //     console.log(`1 : ${results?.gestures[0][0].categoryName}\nright : ${results?.gestures[1][0].categoryName}`)
+    //   }
+    //   } else {
+    //   gestureOutput.style.display = "none";
+    // }
+    // Call this function again to keep predicting when the browser is ready.
+    window.requestAnimationFrame(predictWebcam);
+}
