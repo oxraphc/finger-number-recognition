866736db2b9106ddd376016c2d185b293625dbfc
Oxraphc <caesarravsanjani170@gmail.com>
2025-11-23 15:34:28 +0700

    Add finger number evaluation

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ const drawOverlayContext = drawOverlay.getContext('2d');
 const leftHandCVResultOutput = document.getElementById('left-hand-result');
 const rightHandCVResultOutput = document.getElementById('right-hand-result');
 const inferenceTime = document.getElementById('time-delay-counter');
+const leftValueElem = document.getElementById('left');
+const rightValueElem = document.getElementById('right');
+const operationResult = document.getElementById('result');
 
 let cameraStream = null;
 let gestureRecognizer;
@@ -18,12 +21,10 @@ let lastVideoTime = -1;
 let results = undefined;
 let timeStart;
 let timeEnd;
-let inferenceTimeMs
 
-let left_value = 0;
-let right_value = 0;
+let leftValue = 0;
+let rightValue = 0;
 let operation = '+';
-let operationResult = 0;
 
 
 // Load model
@@ -129,17 +130,68 @@ async function predictWebcam() {
     if (results.gestures.length > 0) {
         for (let i = 0; i < results.gestures.length; i++) {
             const gestureCategory = results.gestures[i][0].categoryName;
+            const gestureCategoryInt = categoryNameToInt(gestureCategory);
             const confidence_score = results.gestures[i][0].score;
             const confidence = Math.trunc(confidence_score * 100) / 100
 
             if (results.handednesses[i][0].index == 0) {
                 rightHandCVResultOutput.innerText = `RIGHT\nGestureCategory: ${gestureCategory}\nConfidence: ${confidence}`
+                rightValueElem.innerText = gestureCategoryInt;
+                rightValue = gestureCategoryInt;
             } else {
                 leftHandCVResultOutput.innerText = `LEFT\nGestureCategory: ${gestureCategory}\nConfidence: ${confidence}`
+                leftValueElem.innerText = gestureCategoryInt;
+                leftValue = gestureCategoryInt;
             }
         }
+
+        operationResult.innerText = evalFingers();
     }
     
     // Call this function again to keep predicting when the browser is ready.
     window.requestAnimationFrame(predictWebcam);
+}
+
+function categoryNameToInt(categoryName) {
+    switch (categoryName) {
+        case 'one':
+            return 1;
+        case 'two':
+            return 2;
+        case 'three':
+            return 3;
+        case 'four':
+            return 4;
+        case 'five':
+            return 5;
+        case 'six':
+            return 6;
+        case 'seven':
+            return 7;
+        case 'eight':
+            return 8;
+        case 'nine':
+            return 9;
+        default:
+            return 0;
+    }
+}
+
+function evalFingers() {
+    switch (operation) {
+        case '+':
+            return leftValue + rightValue;
+        case '-':
+            return leftValue - rightValue;
+        case '*':
+            return leftValue * rightValue;
+        case '/':
+            return leftValue / rightValue;
+        case '^':
+            return leftValue ** rightValue;
+        case '%':
+            return leftValue % rightValue
+        default:
+            return 0;
+    }
 }
